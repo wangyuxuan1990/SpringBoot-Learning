28537732cd780ffb4c9d4c31b41b2fce33cb38ec
wangyuxuan <165140760@qq.com>
2018-10-29 21:06:21 +0800

    Spring Boot与Velocity

---
+++ chapter3-1-4/README.md
@@ -0,0 +1,3 @@
+# Spring Boot开发Web应用
+又换成Velocity了而已。。。
+Spring Boot 2.0好像不支持Velocity。。。
---
+++ chapter3-1-4/src/main/java/com/wangyuxuan/Chapter314Application.java
@@ -0,0 +1,12 @@
+package com.wangyuxuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter314Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter314Application.class, args);
+    }
+}
