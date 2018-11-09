da5e0351f6f083a468e35ad0e8a1a9c883219204
wangyuxuan <165140760@qq.com>
2018-11-09 17:09:31 +0800

    Spring Boot中增强对MongoDB的配置

---
+++ chapter3-2-11/src/main/java/com/wangyuxuan/Chapter3211Application.java
@@ -0,0 +1,14 @@
+package com.wangyuxuan;
+
+import com.spring4all.mongodb.EnableMongoPlus;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableMongoPlus
+@SpringBootApplication
+public class Chapter3211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter3211Application.class, args);
+    }
+}
---
+++ chapter3-2-11/src/test/java/com/wangyuxuan/Chapter3211ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.wangyuxuan;
+
+import com.mongodb.MongoClient;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class Chapter3211ApplicationTests {
+
+    @Autowired
+    private MongoClient mongoClient;
+
+    @Test
+    public void test() throws Exception {
+        log.info("MinConnectionsPerHost = {}, MaxConnectionsPerHost = {}",
+                mongoClient.getMongoClientOptions().getMinConnectionsPerHost(),
+                mongoClient.getMongoClientOptions().getConnectionsPerHost());
+    }
+
+}
