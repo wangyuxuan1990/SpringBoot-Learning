b7fee309d1bb0a96b9064d68f438356829f1805c
wangyuxuan <165140760@qq.com>
2018-11-26 09:51:17 +0800

    一波@Around带走（iKonh要求）

--- chapter4-2-3/src/main/java/com/wangyuxuan/aspect/WebLogAspect.java
+++ chapter4-2-3/src/main/java/com/wangyuxuan/aspect/WebLogAspect.java
@@ -2,10 +2,8 @@ package com.wangyuxuan.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -32,8 +30,31 @@ public class WebLogAspect {
     @Pointcut("execution(public * com.wangyuxuan.web..*.*(..))")
     public void webLog(){}
 
-    @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable{
+//    @Before("webLog()")
+//    public void doBefore(JoinPoint joinPoint) throws Throwable{
+//
+//        startTime.set(System.currentTimeMillis());
+//
+//        // 接收到请求，记录请求内容
+//        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+//        HttpServletRequest request = attributes.getRequest();
+//
+//        // 记录下请求内容
+//        logger.info("URL : " + request.getRequestURL().toString());
+//        logger.info("HTTP_METHOD : " + request.getMethod());
+//        logger.info("IP : " + request.getRemoteAddr());
+//        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+//        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+//    }
+//
+//    @AfterReturning(returning = "ret", pointcut = "webLog()")
+//    public void doAfterReturning(Object ret) throws Throwable{
+//        logger.info("RESPONSE : " + ret);
+//        logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+//    }
+
+    @Around("webLog()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
 
         startTime.set(System.currentTimeMillis());
 
@@ -47,11 +68,12 @@ public class WebLogAspect {
         logger.info("IP : " + request.getRemoteAddr());
         logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
         logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
-    }
 
-    @AfterReturning(returning = "ret", pointcut = "webLog()")
-    public void doAfterReturning(Object ret) throws Throwable{
+        Object ret = joinPoint.proceed();
+
         logger.info("RESPONSE : " + ret);
         logger.info("SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+
+        return ret;
     }
 }
